861e443f06ee1cac32281d315e60bf60b6b1c2df
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 04:43:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,9 @@ def long_planeteer_calls(array)
     return TRUE
     else return false 
 end
+end
 
-def find_the_cheese# code an argument here
+def find_
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
