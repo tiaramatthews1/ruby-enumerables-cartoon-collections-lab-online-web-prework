2f5811eef3badd36a7c84252e769859ed891f25f
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 04:43:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,12 @@ end
 
 def long_planeteer_calls(array)
   if array.any? {|word| word.length > 4}
-    return TRUE
+    return true
     else return false 
 end
 end
 
-def find_
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
