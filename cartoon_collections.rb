f3cab24bf6a374f506d1bf6a07657594b48e3e16
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 04:38:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet# code an argument here
-  array.map { |word| word.capitalize
+  array.map { |word| word.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
