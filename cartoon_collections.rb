d9f9e6998b041c0118967879734b07643df5b78c
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-08 04:42:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any? {|word| word.length > 4
+  if array.any? {|word| word.length > 4}
+    
 end
 
 def find_the_cheese# code an argument here
